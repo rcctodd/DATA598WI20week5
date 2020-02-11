4c80496088125d37a8146a1c7937f8e7ff9b6c2b
Richard Todd <rcctodd@uw.edu>
2020-02-10 21:46:25 -0800

    commit 2

--- R/hello.R
+++ R/hello.R
@@ -16,11 +16,20 @@
 hello <- function() {
   print("Hello, world!")
 }
-usethis::use_mit_license("Ben Marwick")
-usethis::use_version(which = "3.6.0")
+#usethis::use_mit_license("Ben Marwick")
+#usethis::use_version(which = "3.6.0")
 
-usethis::use_package("dplyr",
-                      type = "Imports",
-                      min_version = "0.8.4")
+#usethis::use_vignette("basic_vignette")
 
 
+#usethis::use_readme_md()
+
+#usethis::use_code_of_conduct()
+
+#usethis::use_package("dplyr",
+#                      type = "Imports",
+#                      min_version = "0.8.4")
+
+#usethis::use_package("tidycensus",
+#                     type = "suggests",
+#                     min_version = "1.0.0")
---
+++ README.md
@@ -0,0 +1,32 @@
+
+# DATA598WI20week5
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of DATA598WI20week5 is to ...
+
+## Installation
+
+You can install the released version of DATA598WI20week5 from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("DATA598WI20week5")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(DATA598WI20week5)
+## basic example code
+```
+
+## Licenses
+
+Code is made available under the terms of the MIT license.
+
+Data are made available under the terms of the CC0 license.
+
+Images are made available under the terms of the GPL-2 license.
